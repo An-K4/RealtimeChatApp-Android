e7dbf9770a41ef62aea61db094f311df7e025821
An-K4 <ana5k44nknd@gmail.com>
2026-02-11 11:03:30 +0700

    create app navigation

--- app/src/main/java/com/example/realtimechatapp/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/realtimechatapp/ui/navigation/AppNavigation.kt
@@ -5,7 +5,8 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.realtimechatapp.ui.screens.auth.LoginScreen
-import com.example.realtimechatapp.ui.screens.home.HomeScreen
+import com.example.realtimechatapp.ui.screens.auth.SignupScreen
+import com.example.realtimechatapp.ui.screens.chats.ChatScreen
 
 @Composable
 fun AppNavigation(){
@@ -16,8 +17,12 @@ fun AppNavigation(){
             LoginScreen(navController)
         }
 
+        composable(Screen.Signup.route){
+            SignupScreen(navController)
+        }
+
         composable(Screen.Home.route){
-            HomeScreen(navController)
+            ChatScreen(navController)
         }
     }
 }
--- app/src/main/java/com/example/realtimechatapp/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/realtimechatapp/ui/navigation/Screen.kt
@@ -2,6 +2,7 @@ package com.example.realtimechatapp.ui.navigation
 
 sealed class Screen(val route: String) {
     object Login: Screen("login")
+    object Signup: Screen("signup")
     object Home: Screen("home")
     object Chat: Screen("chat")
     object Account: Screen("account")
