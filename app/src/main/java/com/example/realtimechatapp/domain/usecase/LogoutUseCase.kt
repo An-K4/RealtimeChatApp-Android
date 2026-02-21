2fb52bb138bb64a44d871f21b3eca2ac8c1f3510
An-K4 <ana5k44nknd@gmail.com>
2026-02-21 16:12:46 +0700

    add some logout logic

---
+++ app/src/main/java/com/example/realtimechatapp/domain/usecase/LogoutUseCase.kt
@@ -0,0 +1,10 @@
+package com.example.realtimechatapp.domain.usecase
+
+import com.example.realtimechatapp.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class LogoutUseCase @Inject constructor(private val authRepository: AuthRepository) {
+    suspend operator fun invoke(token: String) : Result<String>{
+        return authRepository.logout(token)
+    }
+}
