6d4108ae785c1a1568ddf06fe2cff32bc1821bb7
An-K4 <ana5k44nknd@gmail.com>
2026-05-06 16:32:52 +0700

    chore: update dependencies and app compat theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,10 +62,13 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.androidx.appcompat)
     implementation("androidx.compose.material:material-icons-extended:1.6.0")
 
     // navigation compose
     implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    implementation("androidx.core:core-splashscreen:1.2.0")
 
     // hilt
     implementation("com.google.dagger:hilt-android:2.51")
