4fc0a71c11e29eb8693353dbb794f2e486377f13
An-K4 <ana5k44nknd@gmail.com>
2026-07-02 16:09:50 +0700

    refactor: rename SimpleResponseDto.kt to MessageResponseDto.kt

--- app/src/main/java/com/example/realtimechatapp/data/remote/api/AuthApi.kt
+++ app/src/main/java/com/example/realtimechatapp/data/remote/api/AuthApi.kt
@@ -4,7 +4,7 @@ import com.example.realtimechatapp.data.remote.dto.user.UserResponseDto
 import com.example.realtimechatapp.data.remote.dto.auth.LoginRequestDto
 import com.example.realtimechatapp.data.remote.dto.auth.LoginResponseDto
 import com.example.realtimechatapp.data.remote.dto.auth.SignupRequestDto
-import com.example.realtimechatapp.data.remote.dto.SimpleResponseDto
+import com.example.realtimechatapp.data.remote.dto.MessageResponseDto
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -14,10 +14,10 @@ interface AuthApi {
     suspend fun login(@Body request: LoginRequestDto): LoginResponseDto
 
     @POST("/auth/signup")
-    suspend fun signup(@Body request: SignupRequestDto): SimpleResponseDto
+    suspend fun signup(@Body request: SignupRequestDto): MessageResponseDto
 
     @POST("/auth/logout")
-    suspend fun logout(): SimpleResponseDto
+    suspend fun logout(): MessageResponseDto
 
     @GET("/auth/me")
     suspend fun getMe(): UserResponseDto
