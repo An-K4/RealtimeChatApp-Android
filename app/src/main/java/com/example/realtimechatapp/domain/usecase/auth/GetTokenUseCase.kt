41b0e6e8880583b808798ad73c786a5febac960f
An-K4 <ana5k44nknd@gmail.com>
2026-07-24 21:04:25 +0700

    fix: add check block to avoid null token

--- app/src/main/java/com/example/realtimechatapp/domain/usecase/auth/GetTokenUseCase.kt
+++ app/src/main/java/com/example/realtimechatapp/domain/usecase/auth/GetTokenUseCase.kt
@@ -1,5 +1,6 @@
 package com.example.realtimechatapp.domain.usecase.auth
 
+import com.example.realtimechatapp.domain.exception.AuthException
 import com.example.realtimechatapp.domain.repository.TokenManager
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
@@ -10,7 +11,11 @@ class GetTokenUseCase @Inject constructor(
     suspend operator fun invoke(): Result<String?> {
         return try {
             val token = tokenManager.token.first()
-            Result.success(token)
+            if (!token.isNullOrEmpty()) {
+                Result.success(token)
+            } else {
+                throw AuthException.UnauthorizedException
+            }
         } catch (e: Exception) {
             Result.failure(e)
         }
