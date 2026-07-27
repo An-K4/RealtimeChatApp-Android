3cab6b0eace87e0041878bbd935cb64990c16fac
An-K4 <ana5k44nknd@gmail.com>
2026-07-27 22:06:46 +0700

    refactor: add group name validator to CreateGroupUseCase.kt

--- app/src/main/java/com/example/realtimechatapp/domain/validation/GroupValidator.kt
+++ app/src/main/java/com/example/realtimechatapp/domain/validation/GroupValidator.kt
@@ -4,14 +4,20 @@ import com.example.realtimechatapp.domain.exception.GroupException
 
 object GroupValidator {
     fun validateGroupIdExist(groupId: String) {
-        if (groupId.isBlank()){
+        if (groupId.isBlank()) {
             throw GroupException.GroupIdNotExistException
         }
     }
 
     fun validateMemberIdExist(memberId: String) {
-        if (memberId.isBlank()){
+        if (memberId.isBlank()) {
             throw GroupException.GroupIdNotExistException
+        }
+    }
+
+    fun validateGroupNameBlank(groupName: String) {
+        if (groupName.isBlank()) {
+            throw GroupException.GroupNameBlankException
         }
     }
 
